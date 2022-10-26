2f80212cde7f9ffd561a06bb8db7d67bec21730f
ansuman nayak <ansumannayak528@gmail.com>
2022-10-26 13:27:03 +0530

    correcttion and some frontend

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ function wbc()
     
     if(x < 52)
     {
-        value.innerHTML = "wbc is low";//
+        value.innerHTML = "wbc is low";
     }
-    else if(x>75)
+    else if(x > 75)
     {
         value.innerHTML = "wbc is high";
     }
@@ -18,19 +18,235 @@ function wbc()
 }
 function rbc()
 {
-    const x = document.getElementById("").value; //this variable get the value of input
-    const value = document.getElementById("");// this variable get the value of output
+    const x = document.getElementById("rbcin").value; //this variable get the value of input
+    const value = document.getElementById("rbcout");// this variable get the value of output
     
     if(x < 20)
     {
         value.innerHTML = "rbc is low";//
     }
-    else if(x>40)
+    else if(x > 40)
     {
         value.innerHTML = "rbc is high";
     }
     else
     {
         value.innerHTML = "rbc is perfect";
+    }
+}
+function Haemoglobin()
+{
+    const x = document.getElementById("").value; //this variable get the value of input
+    const value = document.getElementById("");// this variable get the value of output
+    
+    if(x < 12)
+    {
+        value.innerHTML = "HB is low";//
+    }
+    else if(x > 15)
+    {
+        value.innerHTML = "HB is high";
+    }
+    else
+    {
+        value.innerHTML = "HB is perfect";
+    }
+}
+function HCT()
+{
+    const x = document.getElementById("").value; //this variable get the value of input
+    const value = document.getElementById("");// this variable get the value of output
+    
+    if(x < 36)
+    {
+        value.innerHTML = "HCT is low";//
+    }
+    else if(x > 45)
+    {
+        value.innerHTML = "HCT is high";
+    }
+    else
+    {
+        value.innerHTML = "HCT is perfect";
+    }
+}
+function MCV()
+{
+    const x = document.getElementById("").value; //this variable get the value of input
+    const value = document.getElementById("");// this variable get the value of output
+    
+    if(x < 83)
+    {
+        value.innerHTML = "MCV is low";//
+    }
+    else if(x > 101)
+    {
+        value.innerHTML = "MCV is high";
+    }
+    else
+    {
+        value.innerHTML = "MCV is perfect";
+    }
+}
+function MCH()
+{
+    const x = document.getElementById("").value; //this variable get the value of input
+    const value = document.getElementById("");// this variable get the value of output
+    
+    if(x < 27)
+    {
+        value.innerHTML = "MCH is low";//
+    }
+    else if(x > 32)
+    {
+        value.innerHTML = "MCH is high";
+    }
+    else
+    {
+        value.innerHTML = "MCH is perfect";
+    }
+}
+function MCHC()
+{
+    const x = document.getElementById("").value; //this variable get the value of input
+    const value = document.getElementById("");// this variable get the value of output
+    
+    if(x < 32)
+    {
+        value.innerHTML = "MCHC is low";//
+    }
+    else if(x > 35)
+    {
+        value.innerHTML = "MCHC is high";
+    }
+    else
+    {
+        value.innerHTML = "MCHC is perfect";
+    }
+}
+function RDW()
+{
+    const x = document.getElementById("").value; //this variable get the value of input
+    const value = document.getElementById("");// this variable get the value of output
+    
+    if(x < 12)
+    {
+        value.innerHTML = "RDW is low";//
+    }
+    else if(x > 14)
+    {
+        value.innerHTML = "RDW is high";
+    }
+    else
+    {
+        value.innerHTML = "RDW is perfect";
+    }
+}
+function Platelet_Count()
+{
+    const x = document.getElementById("").value; //this variable get the value of input
+    const value = document.getElementById("");// this variable get the value of output
+    
+    if(x < 2)
+    {
+        value.innerHTML = "PC is low";//
+    }
+    else if(x > 4)
+    {
+        value.innerHTML = "PC is high";
+    }
+    else
+    {
+        value.innerHTML = "PC is perfect";
+    }
+}
+function Neutrophil()
+{
+    const x = document.getElementById("").value; //this variable get the value of input
+    const value = document.getElementById("");// this variable get the value of output
+    
+    if(x < 40)
+    {
+        value.innerHTML = "NC is low";//
+    }
+    else if(x > 80)
+    {
+        value.innerHTML = "NC is high";
+    }
+    else
+    {
+        value.innerHTML = "NC is perfect";
+    }
+}
+function Eosinophil()
+{
+    const x = document.getElementById("").value; //this variable get the value of input
+    const value = document.getElementById("");// this variable get the value of output
+    
+    if(x < 1)
+    {
+        value.innerHTML = "EC is low";//
+    }
+    else if(x > 6)
+    {
+        value.innerHTML = "EC is high";
+    }
+    else
+    {
+        value.innerHTML = "EC is perfect";
+    }
+}
+function Basophil()
+{
+    const x = document.getElementById("").value; //this variable get the value of input
+    const value = document.getElementById("");// this variable get the value of output
+    
+    if(x < 1)
+    {
+        value.innerHTML = "BC is low";//
+    }
+    else if(x > 2)
+    {
+        value.innerHTML = "BC is high";
+    }
+    else
+    {
+        value.innerHTML = "BC is perfect";
+    }
+}
+function Lymphocyte()
+{
+    const x = document.getElementById("").value; //this variable get the value of input
+    const value = document.getElementById("");// this variable get the value of output
+    
+    if(x < 20)
+    {
+        value.innerHTML = "LC is low";//
+    }
+    else if(x > 40)
+    {
+        value.innerHTML = "LC is high";
+    }
+    else
+    {
+        value.innerHTML = "LC is perfect";
+    }
+}
+function Monocyte()
+{
+    const x = document.getElementById("").value; //this variable get the value of input
+    const value = document.getElementById("");// this variable get the value of output
+    
+    if(x < 2)
+    {
+        value.innerHTML = "MC is low";//
+    }
+    else if(x > 10)
+    {
+        value.innerHTML = "MC is high";
+    }
+    else
+    {
+        value.innerHTML = "MC is perfect";
     }
 }
